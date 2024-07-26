dc276364d45e99d2df29f1f612f0b147721181e3
ggang89 <ggang89@gmail.com>
2024-07-26 17:06:29 +0900

    PropTypes설정=> npm i prop-types 후 import해서 사용하기 *P대소문자 구분*

--- src/Movie.js
+++ src/Movie.js
@@ -1,15 +1,23 @@
-export default function Movie({img,title,summary,genres}) {
-  
+import PropTypes from "prop-types";
+
+export default function Movie({ img, title, summary, genres }) {
   return (
     <div>
       <img src={img} alt={title}></img>
       <h2>{title}</h2>
       <p>{summary}</p>
       <ul>
-        {genres.map(g => (
+        {genres.map((g) => (
           <li key={g}>{g}</li>
         ))}
       </ul>
     </div>
-  )
+  );
 }
+
+Movie.prototype = {
+  img: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
