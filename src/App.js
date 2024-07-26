2b205db1fc7c2d1734563f64cf2db68a78883749
ggang89 <ggang89@gmail.com>
2024-07-26 16:08:21 +0900

    fetch()쓰는 법(await 2개로 호출) : 노마드 방법=> 일반적인 방법으로 변경

--- src/App.js
+++ src/App.js
@@ -6,17 +6,29 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
 
+  //노마드 수업
+  // const getMovies = async () => {
+  //   const json = await (
+  //     await fetch(
+  //       `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+  //     )
+  //   ).json();
+  //   setLoading(false);
+  //   setMovies(json.data.movies);
+  // };
+
+  //기본 fetch 호출법
   const getMovies = async () => {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-      )
-    ).json();
+    const response = await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+    );
+    const json = await response.json();
     setLoading(false);
     setMovies(json.data.movies);
   };
+
   useEffect(() => {
-    getMovies();
+    getMovies(); //모든 렌더링 후에 실행되는 코드
   }, []);
   console.log(movies);
 
